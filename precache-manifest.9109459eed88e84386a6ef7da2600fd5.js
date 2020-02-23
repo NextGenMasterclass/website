bed83f8e47c21de61ead316a03d4d0e925e340bc
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-02-22 17:03:06 -0700

    Updates

---
+++ precache-manifest.9109459eed88e84386a6ef7da2600fd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443803ab7c98b0620b7fc53bd66ff166",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3dfb4155feddb94a661c",
+    "url": "./static/css/main.e560c158.chunk.css"
+  },
+  {
+    "revision": "e932406e3668dbe18118",
+    "url": "./static/js/2.e86cb0c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.e86cb0c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3dfb4155feddb94a661c",
+    "url": "./static/js/main.f1694a61.chunk.js"
+  },
+  {
+    "revision": "0597ee1c1de87388366e",
+    "url": "./static/js/runtime-main.a2d298a2.js"
+  },
+  {
+    "revision": "a68901d6d8585f2d4f6d0b113c30f3c6",
+    "url": "./static/media/p1.a68901d6.jpg"
+  }
+]);
